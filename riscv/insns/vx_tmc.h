0b3ede9e7f64d889eeefde13e7361ffe302cc34b
Hansung Kim <hansung_kim@berkeley.edu>
2023-10-05 01:15:41 -0700

    Fix wrong printf in vx_tmc

--- riscv/insns/vx_tmc.h
+++ riscv/insns/vx_tmc.h
@@ -13,6 +13,10 @@ if (LANE == leftmost) {
     reg_t bitmask = (1ul << i);
     STATE.thread_mask[i] = ((bitmask & RS1) != 0ul);
   }
+  fprintf(stderr, "warp: %u: set thread_mask: ", p->get_id());
+  for (unsigned int i = 0; i < NUM_THREADS; i++) {
+    fprintf(stderr, "%d ", STATE.thread_mask[i] ? 1 : 0);
+  }
   // if all threads got deactivated, deactivate this warp
   bool all_inactive = true;
   for (unsigned int i = 0; i < NUM_THREADS; i++) {
@@ -21,12 +25,9 @@ if (LANE == leftmost) {
       break;
     }
   }
+  fprintf(stderr, "\n");
   if (all_inactive) {
     p->spawn_warp(p->get_id(), false, 0 /* dontcare */);
+    fprintf(stderr, "warp: %u: all lanes inactive, despawning warp\n", p->get_id());
   }
-  fprintf(stderr, "warp: %u: changed thread_mask: ", p->get_id());
-  for (int i = 0; i < NUM_THREADS; i++) {
-    fprintf(stderr, "%d ", STATE.thread_mask[i] ? 1 : 0);
-  }
-  fprintf(stderr, "\n");
 }
